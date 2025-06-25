616fb15931fe790bd6401c1ae5f32c18e8a32eef
ralphy <raphael.marandet@gmail.com>
2025-06-25 11:57:51 +0200

    Creating Playlist Control Class

---
+++ doc/playlist-management.md
@@ -0,0 +1,17 @@
+# play list management
+
+## Fonctionnement imaginé
+
+### Programme par defaut
+
+- on définit un `programme-par-defaut`.
+- on lance ce `programme-par-defaut` au démarrage du process radio.
+- lorsque ce `programme-par-defaut` s'arrête, on le relance.
+ 
+### Programmes spéciaux
+
+- à différents horaires selon le contenu du `cron-tab`, j'interromps le `programme-par-defaut` et je lance un `programme-spécial`.
+- lorsqu'un `programme-spécial` s'arrête, je retourne au `programme-par-defaut`.
+- ces horaires sont définis dans un système `cron-tab` applicatif
+
+
--- src/libs/vlc-control/VLCControl.js
+++ src/libs/vlc-control/VLCControl.js
@@ -122,32 +122,6 @@ class VLCControl {
         return data.heap;
     }
 
-    shuffleArray (aArray) {
-        // Créer une copie du tableau pour éviter de modifier l'original
-        const shuffled = [...aArray];
-
-        // Parcourir le tableau à l'envers
-        for (let i = shuffled.length - 1; i > 0; i--) {
-            // Générer un index aléatoire entre 0 et i (inclus)
-            const j = Math.floor(Math.random() * (i + 1));
-
-            // Échanger les éléments aux indices i et j
-            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-        }
-
-        return shuffled;
-    }
-
-    getFolderContent(sBasePath, aExtensions = []) {
-        const aLowerExts = aExtensions.map(s => '.' + s.toLowerCase());
-        return TreeAsync
-            .tree(sBasePath)
-            .then(aFiles => aFiles
-                .filter(s => aLowerExts.length === 0 || aLowerExts.includes(path.extname(s).toLowerCase()))
-                .map(s => path.resolve(sBasePath, s))
-            );
-    }
-
     renderSwitchValue (sSwitch, b) {
         return sSwitch + ' ' + (b ? 'on' : 'off');
     }
